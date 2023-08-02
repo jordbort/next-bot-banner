e9fcac22a40c18a8b67fc9375e17b20b190bbab8
jordbort <jordan.bortner@gmail.com>
2023-08-01 20:50:22 -0400

    First commit, cleaning up boilerpate code

---
+++ src/app/banlist/page.tsx
@@ -0,0 +1,10 @@
+import GeneratedList from "@/components/GeneratedList";
+
+export default function BanList() {
+    return (
+        <main>
+            <h1>Ban List</h1>
+            <GeneratedList />
+        </main>
+    )
+}
---
+++ src/components/GeneratedList.tsx
@@ -0,0 +1,3 @@
+export default function GeneratedList() {
+    return <h2>list goes here</h2>
+}
