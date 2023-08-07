7d42baf7b1817f28e585a77412c8e67b351b9041
jordbort <jordan.bortner@gmail.com>
2023-08-07 01:42:16 -0400

    Adding nav bar and about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <main>
+            <h1 className="page-title">About page</h1>
+        </main>
+    )
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+// import Router from "next/router";
+
+export default function NavBar() {
+    return <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+    </nav>
+}
