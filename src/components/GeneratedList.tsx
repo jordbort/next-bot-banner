52a7c7721681b9ce0e2e87e4487278ef7e9bd139
jordbort <jordan.bortner@gmail.com>
2023-11-07 23:17:35 -0500

    Adding banned-list option for local storage

--- src/components/GeneratedList.tsx
+++ src/components/GeneratedList.tsx
@@ -6,10 +6,13 @@ interface Props {
 
 export default function GeneratedList({ checkedBots }: Props) {
     const [results, setResults] = useState<string[]>([])
-    const [checked, setChecked] = useState<boolean>(false)
+    const [recentChecked, setRecentChecked] = useState<boolean>(false)
+    const [filterChecked, setFilterChecked] = useState<boolean>(true)
     const [loadingText, setLoadingText] = useState<string>('Click the button to generate your ban list!')
 
     const maxDaysActive: number = 14
+    const bannedList: string | null = localStorage.getItem(`banned-list`)
+    const rememberedBots: string[] = bannedList ? bannedList.split(`,`) : []
 
     function fetchBots() {
         setResults([])
@@ -19,15 +22,13 @@ export default function GeneratedList({ checkedBots }: Props) {
             .then(response => response.json())
             .then(json => {
                 const newArr: string[] = []
-                json.bots.forEach((entry: [botName: string, channels: number, num: number]) => {
+                const day = 86400
+                const activePeriod = Math.floor(Date.now() / 1000) - (day * maxDaysActive)
+                json.bots.map((entry: [botName: string, channels: number, num: number]) => {
                     if (!checkedBots.includes(entry[0])) {
-                        if (checked) {
-                            const day = 86400
-                            const activePeriod = Math.floor(Date.now() / 1000) - (day * maxDaysActive)
-                            entry[2] >= activePeriod && newArr.push(entry[0])
-                        } else {
-                            newArr.push(entry[0])
-                        }
+                        if (filterChecked && rememberedBots.includes(entry[0])) { return }
+                        if (recentChecked && entry[2] < activePeriod) { return }
+                        newArr.push(entry[0])
                     }
                 })
                 setLoadingText('')
@@ -39,13 +40,24 @@ export default function GeneratedList({ checkedBots }: Props) {
         <>
             <div>
                 <label htmlFor="recent">Only get bots active in the past {maxDaysActive} {maxDaysActive === 1 ? `day` : `days`}?</label>
-                <input type="checkbox" id="recent" checked={checked} onChange={() => setChecked(!checked)} />
+                <input type="checkbox" id="recent" checked={recentChecked} onChange={() => setRecentChecked(!recentChecked)} />
             </div>
-            <button onClick={fetchBots}>Generate List</button>
+
+            {rememberedBots.length > 0 &&
+                <div>
+                    <label htmlFor="filter">Exclude {rememberedBots.length.toLocaleString()} remembered bots?</label>
+                    <input type="checkbox" id="filter" checked={filterChecked} onChange={() => setFilterChecked(!filterChecked)} />
+                </div>
+            }
+
+            <button className="generate-list-button" onClick={fetchBots}>Generate List</button>
+
             <div className="margin-top-16">
                 {results.length ? <>
                     <h4>Total usernames in ban list: {results.length.toLocaleString()}</h4>
                     <button className="copy-button" onClick={() => navigator.clipboard.writeText(results.join(`\n`))}>Copy List</button>
+                    <button className="storage-button" onClick={() => localStorage.setItem(`banned-list`, results.toString())}>Remember List</button>
+                    <button className="clear-button" onClick={() => localStorage.removeItem(`banned-list`)}>Clear List</button>
                     <ul>
                         {results.map((robot, idx) => <li key={idx}>{robot}</li>)}
                     </ul>
